c26e99d84e2e2d1bfb6bacc0e5fd808da1340469
sankalp <sankalpyerigeri20@gmail.com>
2024-12-11 20:04:58 +0530

    Developed an API to respond to the requests

--- src/routes/requests.js
+++ src/routes/requests.js
@@ -12,13 +12,66 @@ requestsRouter.post("/connectionRequest",userAuth, (req, res)=>{
     res.send("Connection request sent by " + user._id);
 })
 
+requestsRouter.post("/request/review/:status/:requestId",userAuth, async (req,res)=>
+{
+    try
+    {
+        const reqId = req.params.requestId;
+        const status = req.params.status;
+        const loggedInUser = req.user._id;
+
+        const allowedUpdates = ["accepted", "rejected"]
+
+        if(!allowedUpdates.includes(status))
+        {
+            throw new Error("Invalid request")
+        }
+
+        const connectionReq = await connectionRequest.findOne({
+            _id: reqId,
+            toUserId: loggedInUser,
+            status: "interested"
+        })
+
+        if(!connectionReq)
+        {
+            throw new Error("Invalid ID apa")
+        }
+
+        connectionReq.status = status
+        const data = await connectionReq.save()
+        res.json(
+            {
+                message: "Conncection " + status + "ed" + " Successfully",
+                data
+            }
+        )
+
+
+
+    }
+    catch(err)
+    {
+        res.status(400).send("ERROR " + err.message)
+    }
+})
+
 
 requestsRouter.post("/request/send/:status/:toUserId", userAuth, async (req, res)=>
 {
     try{
     const fromUserId = req.user._id;
+    
     const toUserId = req.params.toUserId;
+    
     const status = req.params.status;
+
+    const allowedStatus = ["interested", "ignored"]
+
+    if(!allowedStatus.includes(status))
+    {
+        throw new Error("This type of status not allowed")
+    }
 
     const toUser = await User.findById(toUserId)
     
@@ -27,7 +80,7 @@ requestsRouter.post("/request/send/:status/:toUserId", userAuth, async (req, res
         return res.status(400).send("no such User found")
     }
 
-    const connectionReqExist = connectionRequest.find({
+    const connectionReqExist = await connectionRequest.findOne({
         $or: [
             {fromUserId, toUserId},
             {
@@ -36,6 +89,8 @@ requestsRouter.post("/request/send/:status/:toUserId", userAuth, async (req, res
             }
         ]
     })
+
+    
 
     if(connectionReqExist)
     {
